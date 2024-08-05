e46a221b68b29378160a4443f3341a1383d3d0c9
heyolaniran <olaniran.abd@gmail.com>
2024-08-05 19:57:07 +0100

    fix: nav bar main menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,38 +1,49 @@
 import { leftLinks, rightLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import SubNav from "./ui/SubNav";
 export default function NavBar() {
   return (
-    <nav className=" px-16 pb-4  pt-4 text-black">
-      <div className="flex items-center border pb-4 border-t-0 border-l-0 border-r-0 flew justify-between ">
-        <div className="">
-          {leftLinks.map((link) => (
-            <Link key={link.id} href={link.href} className="px-2">
-              {link.name}
-            </Link>
-          ))}
-        </div>
+    <>
+      <nav className=" px-16 pb-4 w-full pt-4 lg:block hidden text-black">
+        <div className=" flex items-center border pb-4 border-t-0 border-l-0 border-r-0 flew justify-between ">
+          <div className="">
+            {leftLinks.map((link) => (
+              <Link key={link.id} href={link.href} className="px-2">
+                {link.name}
+              </Link>
+            ))}
+          </div>
 
-        <div className="">
-          <Image src={"/logo.svg"} width={150} height={150} alt="" />
-        </div>
+          <div className="">
+            <Image src={"/logo.svg"} width={150} height={150} alt="" />
+          </div>
 
-        <div className="flex items-center">
-          {rightLinks.map((link) => (
-            <Link key={link.id} href={link.href} className="px-2">
-              {link.name === "Account" ? (
-                <span className="gap-2 flex items-center">
-                  {" "}
-                  <Image src={"/user.svg"} width={18} height={18} alt="" />{" "}
-                  {link.name}{" "}
-                </span>
-              ) : (
-                link.name
-              )}
-            </Link>
-          ))}
+          <div className="flex items-center">
+            {rightLinks.map((link) => (
+              <Link key={link.id} href={link.href} className="px-2">
+                {link.name === "Account" ? (
+                  <span className="gap-2 flex items-center">
+                    {" "}
+                    <Image
+                      src={"/user.svg"}
+                      width={18}
+                      height={18}
+                      alt=""
+                    />{" "}
+                    {link.name}{" "}
+                  </span>
+                ) : (
+                  link.name
+                )}
+              </Link>
+            ))}
+            <Image src={"/search.svg"} width={20} height={20} alt="s" />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <SubNav />
+    </>
   );
 }
---
+++ src/components/ui/SubNav.tsx
@@ -0,0 +1,34 @@
+"use client";
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function SubNav() {
+  const [open, setIsOpen] = useState<boolean>(false);
+  return (
+    <nav className="px-4 py-2 lg:hidden block">
+      <div className="flex items-center justify-between px-6">
+        
+        <div className="">
+            <Image src={open ? '/close.svg' : '/menu.svg'} width={42} height={20} alt="" className={`${open? ' transition ease-in duration-300' : ' transition-all ease-linear duration-300'}`} onClick={() => setIsOpen(!open)} />
+        </div>
+
+        <div className="">
+          <Image src={ "/logo.svg"} width={150} height={150} alt="" />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Image src={"/cart.svg"} width={20} height={20} alt="c" />
+          <Image src={"/search.svg"} width={20} height={20} alt="s" />
+        </div>
+      </div>
+
+      <div className={clsx(
+        `relative  h-1/2 text-white lg:hidden bg-black/70 top-0 w-full -translate-y-full transition-all ${open? '': 'hidden'}`, open && "translate-y-0 "
+      )}>
+        lorem
+      </div>
+    </nav>
+  );
+}
