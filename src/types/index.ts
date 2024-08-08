00cfaf7864d81af533ec12d4372463790fb16dc6
heyolaniran <olaniran.abd@gmail.com>
2024-08-08 23:25:41 +0100

    feat: product structure for integration

--- src/types/index.ts
+++ src/types/index.ts
@@ -60,10 +60,37 @@ export type ShoppingMethodProps = {
 }
 
 export type CartProductProps = {
+    id: string
     name: string
     color: string
     size: string
     picture: string
     amount: number
     currency: string
+}
+
+export type ProductCollectionProps = {
+    id: string
+    picture: string
+    name: string
+    price: number
+    currency: string
+}
+
+export  type ProductDetailProps = {
+    id : string
+    picture : string
+    title : string
+    description: string
+    color: string 
+    variant: ProductVariantProps[]
+    
+}
+
+export type CheckoutProductProps =  ProductCollectionProps 
+
+type ProductVariantProps = {
+    id: string
+    title: string
+    picture: string
 }
