12f8ecd9fdcc39d11161a6c15a065a9c378be17f
heyolaniran <olaniran.abd@gmail.com>
2024-08-12 23:51:42 +0100

    fix: padding on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="container px-14">
+    <main className="container px-8">
       <Hero />
       <Category />
       <Collection />
